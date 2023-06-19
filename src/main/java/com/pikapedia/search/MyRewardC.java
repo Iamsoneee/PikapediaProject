518b06d0aba6b11efcc68f923ac1539cb8355b47
ILOVEGOM <does0539@naver.com>
2023-06-19 11:47:11 +0900

    MyRewardC import 수정

--- src/main/java/com/pikapedia/search/MyRewardC.java
+++ src/main/java/com/pikapedia/search/MyRewardC.java
@@ -7,12 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.jp.account.AccountDAO;
+import com.pikapedia.account.AccountDAO;
+
 
 @WebServlet("/MyRewardC")
 public class MyRewardC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		com.pikapedia.account.AccountDAO.checkLogin(request);
+		AccountDAO.checkLogin(request);
 		SearchDAO.getAllPokemon(request);
 		request.setAttribute("contentPage", "myRewards.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
