a00466bc8438dd9e74ac500b09a4c3aaeaf1b4b8
Soneee <sowon0512@gmail.com>
2023-06-21 12:31:08 +0900

    [Feat] 메인 페이지 Dark Mode 기능 구현 (클릭 이벤트 시 아이콘 변경, 페이지 내 이동 시에도 다크모드 상태 유지 - index.js)

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,21 +1,51 @@
-$(document).ready(function () {
-  var darkModeEnabled = false;
+// document.addEventListener("DOMContentLoaded", function () {
+//   var body = document.body;
+//   var moonIcon = document.getElementById("dark-mode-toggle");
+//   var sunIcon = document.getElementById("bright-mode-toggle");
 
-  $("#dark-mode-toggle").click(function () {
-    darkModeEnabled = true;
-    enableDarkMode();
-  });
+//   moonIcon.addEventListener("click", function () {
+//     body.classList.add("dark-mode");
+//     moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
+//     sunIcon.setAttribute("src", "img/icon/sun-icon.png");
+//   });
 
-  $("#bright-mode-toggle").click(function () {
-    darkModeEnabled = false;
-    enableBrightMode();
-  });
+//   sunIcon.addEventListener("click", function () {
+//     body.classList.remove("dark-mode");
+//     moonIcon.setAttribute("src", "img/icon/moon-icon.svg");
+//     sunIcon.setAttribute("src", "img/icon/sun-icon-active.png");
+//   });
+// });
 
-  function enableDarkMode() {
-    $("body").addClass("dark-mode");
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.body;
+  var moonIcon = document.getElementById("dark-mode-toggle");
+  var sunIcon = document.getElementById("bright-mode-toggle");
+
+  // 로컬 스토리지에서 다크 모드 설정을 가져옵니다.
+  var isDarkMode = localStorage.getItem("darkMode");
+
+  // 페이지 로드 시 이전에 저장된 다크 모드 설정을 확인하고 적용합니다.
+  if (isDarkMode === "true") {
+    body.classList.add("dark-mode");
+    moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
+    sunIcon.setAttribute("src", "img/icon/sun-icon.png");
   }
 
-  function enableBrightMode() {
-    $("body").removeClass("dark-mode");
-  }
+  moonIcon.addEventListener("click", function () {
+    // 다크 모드 설정을 로컬 스토리지에 저장합니다.
+    localStorage.setItem("darkMode", "true");
+
+    body.classList.add("dark-mode");
+    moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
+    sunIcon.setAttribute("src", "img/icon/sun-icon.png");
+  });
+
+  sunIcon.addEventListener("click", function () {
+    // 다크 모드 설정을 로컬 스토리지에서 제거합니다.
+    localStorage.removeItem("darkMode");
+
+    body.classList.remove("dark-mode");
+    moonIcon.setAttribute("src", "img/icon/moon-icon.svg");
+    sunIcon.setAttribute("src", "img/icon/sun-icon-active.png");
+  });
 });
