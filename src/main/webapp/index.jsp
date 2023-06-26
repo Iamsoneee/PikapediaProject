faa2008d5f4e6982e6641cb86a7d96a8395b2cc4
Soneee <sowon0512@gmail.com>
2023-06-26 17:24:43 +0900

    [Design] index.jsp 헤더 영역 반응형 적용 및 drop-down 메뉴 구현 완료

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -66,6 +66,7 @@ pageEncoding="UTF-8"%>
             </li>
           </ul>
           <!-- Dropdown Menu -->
+          <div>
           <ul class="drop-menu">
             <li><a href="GameHomeC">Quiz</a></li>
             <li><a href="MyRewardC">MyRewards</a></li>
@@ -73,12 +74,12 @@ pageEncoding="UTF-8"%>
             <li><a href="LoginC">Login</a></li>
             <li><a href="SignupC">SignupC</a></li>
             <li><a href="jsp/mypage.jsp">Mypage</a></li>
-            <li> <a href="" class="drop-icon-area"
-            ><img class="drop-icon-img" src="img/icon/list-icon.png" alt=""
-          /></a></li>
 <%--            mypage 링크 컨트롤러 만들면 바꿔주세요--%>
           </ul>
-         
+            <a href="" class="drop-icon-area"
+            ><img class="drop-icon-img" src="img/icon/list-icon.png" alt=""
+          /></a>
+         </div>
         </div>
       </div>
     </header>
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,43 +1,56 @@
 // ************ DARK MODE JS ************
 
-document.addEventListener("DOMContentLoaded", function () {
-  var body = document.body;
-  var moonIcon = document.getElementById("dark-mode-toggle");
-  var sunIcon = document.getElementById("bright-mode-toggle");
+document.addEventListener("DOMContentLoaded", function() {
+	var body = document.body;
+	var moonIcon = document.getElementById("dark-mode-toggle");
+	var sunIcon = document.getElementById("bright-mode-toggle");
 
-  // 로컬 스토리지에서 다크 모드 설정을 가져옵니다.
-  var isDarkMode = localStorage.getItem("darkMode");
+	// 로컬 스토리지에서 다크 모드 설정을 가져옵니다.
+	var isDarkMode = localStorage.getItem("darkMode");
 
-  // 페이지 로드 시 이전에 저장된 다크 모드 설정을 확인하고 적용합니다.
-  if (isDarkMode === "true") {
-    body.classList.add("dark-mode");
-    moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
-    sunIcon.setAttribute("src", "img/icon/sun-icon.png");
-  }
+	// 페이지 로드 시 이전에 저장된 다크 모드 설정을 확인하고 적용합니다.
+	if (isDarkMode === "true") {
+		body.classList.add("dark-mode");
+		moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
+		sunIcon.setAttribute("src", "img/icon/sun-icon.png");
+	}
 
-  moonIcon.addEventListener("click", function () {
-    // 다크 모드 설정을 로컬 스토리지에 저장합니다.
-    localStorage.setItem("darkMode", "true");
+	moonIcon.addEventListener("click", function() {
+		// 다크 모드 설정을 로컬 스토리지에 저장합니다.
+		localStorage.setItem("darkMode", "true");
 
-    body.classList.add("dark-mode");
-    moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
-    sunIcon.setAttribute("src", "img/icon/sun-icon.png");
-  });
+		body.classList.add("dark-mode");
+		moonIcon.setAttribute("src", "img/icon/moon-icon-active.png");
+		sunIcon.setAttribute("src", "img/icon/sun-icon.png");
+	});
 
-  sunIcon.addEventListener("click", function () {
-    // 다크 모드 설정을 로컬 스토리지에서 제거합니다.
-    event.preventDefault(); // 클릭 이벤트의 기본 동작 중지
-    localStorage.removeItem("darkMode");
+	sunIcon.addEventListener("click", function() {
+		// 다크 모드 설정을 로컬 스토리지에서 제거합니다.
+		event.preventDefault(); // 클릭 이벤트의 기본 동작 중지
+		localStorage.removeItem("darkMode");
 
-    body.classList.remove("dark-mode");
-    moonIcon.setAttribute("src", "img/icon/moon-icon.svg");
-    sunIcon.setAttribute("src", "img/icon/sun-icon-active.png");
-  });
-  
-  const dropIcon = document.querySelector('.drop-icon-img');
-  const dropMenu = document.querySelector('.drop-menu');
-  dropIcon.addEventListener('click', () => {
-	dropMenu.classList.toggle('active');
-});
-  
+		body.classList.remove("dark-mode");
+		moonIcon.setAttribute("src", "img/icon/moon-icon.svg");
+		sunIcon.setAttribute("src", "img/icon/sun-icon-active.png");
+	});
+
+	/* Drop Down Menu JS */
+
+	const dropIcon = document.querySelector('.drop-icon-img');
+	const dropMenu = document.querySelector('.drop-menu');
+	dropIcon.addEventListener('click', () => {
+		event.preventDefault(); // 새로고침 방지
+		dropMenu.classList.toggle('active');
+		if (dropMenu.classList.contains('active')) {
+			dropMenu.style.maxHeight = dropMenu.scrollHeight + 'px';
+		} else {
+			dropMenu.style.maxHeight = '0';
+		}
+	});
+
+	dropMenu.addEventListener('mouseleave', () => {
+		dropMenu.classList.remove('active');
+		dropMenu.style.maxHeight = '0';
+	});
+
 });
