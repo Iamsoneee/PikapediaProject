4921402dd2a7e3ef5c77333e15b44517e377db13
Soneee <sowon0512@gmail.com>
2023-07-04 16:57:15 +0900

    [Fix] Azure 배포를 위한 전자지갑 경로 수정 및 LoginC 인코딩 오류 수정

--- src/main/java/com/pikapedia/account/LoginC.java
+++ src/main/java/com/pikapedia/account/LoginC.java
@@ -16,6 +16,7 @@ public class LoginC extends HttpServlet {
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
 		AccountDAO.Login(request);
 		DBDAO.getAllPokemon(request);
 		DBDAO.getAllColor(request);
